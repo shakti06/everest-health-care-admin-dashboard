b7e5f0431bdffcd6a1e94d1c03e28fab5302e72e
shakti06 <shaktibhujel06@gmail.com>
2023-11-20 14:30:51 +1100

    constant file for menu added

---
+++ app/constant/items.ts
@@ -0,0 +1,26 @@
+export const items = [
+    {
+        label : "Dashboard",
+        path : "/",
+        icon : ""
+    },
+    {
+        label : "Patient",
+        path : "/patient",
+        icon : "",
+        patients : [
+            {
+                label : "Patient List",
+                path : "/patient-list",
+            },
+            {
+                label : "Patient Profile",
+                path : "/patient-profile",
+            },
+            {
+                label : "Add New Patient",
+                path : "/add-new-patient",
+            }
+        ]
+    }
+]
